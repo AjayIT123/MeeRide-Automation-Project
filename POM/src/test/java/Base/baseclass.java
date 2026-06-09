524ef691d1e09f6505d5efd3e57f8664075ddafe
AJAY <ajayjampana1993@gmail.com>
2026-06-09 23:39:12 +0530

    Verify WebDriver Setup

---
+++ POM/src/test/java/Base/baseclass.java
@@ -0,0 +1,25 @@
+package Base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class baseclass {
+	public WebDriver driver;
+	@BeforeClass
+	public void setup() {
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://app.themeeride.com/_flash");
+	}
+	
+	@AfterMethod
+	public void teardown() {
+		//driver.quit();
+	}
+}
